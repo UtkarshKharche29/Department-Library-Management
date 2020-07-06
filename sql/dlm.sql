301a70e7c52333840e4336158529fe50ccf9acda
Diksha Nigam <nigamdiksha02@gmail.com>
2020-07-06 17:39:06 +0530

    update changes in dlm.sql

--- sql/dlm.sql
+++ sql/dlm.sql
@@ -1,15 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 01, 2020 at 10:22 AM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.4.7
+-- Host: localhost:3306
+-- Generation Time: Jul 06, 2020 at 12:06 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT
+= 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,21 +31,30 @@ SET time_zone = "+00:00";
 -- Table structure for table `books`
 --
 
-CREATE TABLE `books` (
-  `Book_ID` varchar(30) NOT NULL,
-  `Book_Name` varchar(80) NOT NULL,
-  `Author_Name` varchar(80) NOT NULL,
-  `Publication` varchar(80) NOT NULL,
-  `Available` int(30) NOT NULL
+CREATE TABLE `books`
+(
+  `Book_ID` varchar
+(30) NOT NULL,
+  `Book_Name` varchar
+(80) NOT NULL,
+  `Author_Name` varchar
+(80) NOT NULL,
+  `Publication` varchar
+(80) NOT NULL,
+  `issue_status` int
+(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `books`
 --
 
-INSERT INTO `books` (`Book_ID`, `Book_Name`, `Author_Name`, `Publication`, `Available`) VALUES
-('1', 'Computer Network', 'A.S. Tanenbaum', 'Pearson Education', 18),
-('2', 'Data Warehousing ', 'Reema Theraja', 'Oxford University Press', 17);
+INSERT INTO `books` (`
+Book_ID`,
+`Book_Name
+`, `Author_Name`, `Publication`, `issue_status`) VALUES
+('1', 'Computer Network', 'A.S. Tanenbaum', 'Pearson Education', 1),
+('2', 'Data Warehousing ', 'Reema Theraja', 'Oxford University Press', 1);
 
 -- --------------------------------------------------------
 
@@ -49,20 +62,30 @@ INSERT INTO `books` (`Book_ID`, `Book_Name`, `Author_Name`, `Publication`, `Avai
 -- Table structure for table `faculty`
 --
 
-CREATE TABLE `faculty` (
-  `SDRN_No` varchar(30) NOT NULL,
-  `First_Name` varchar(30) NOT NULL,
-  `Middle_Name` varchar(30) NOT NULL,
-  `Last_Name` varchar(30) NOT NULL,
-  `Email_ID` varchar(30) NOT NULL,
-  `Contact_No` bigint(10) NOT NULL
+CREATE TABLE `faculty`
+(
+  `SDRN_No` varchar
+(30) NOT NULL,
+  `First_Name` varchar
+(30) NOT NULL,
+  `Middle_Name` varchar
+(30) NOT NULL,
+  `Last_Name` varchar
+(30) NOT NULL,
+  `Email_ID` varchar
+(30) NOT NULL,
+  `Contact_No` bigint
+(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `faculty`
 --
 
-INSERT INTO `faculty` (`SDRN_No`, `First_Name`, `Middle_Name`, `Last_Name`, `Email_ID`, `Contact_No`) VALUES
+INSERT INTO `faculty` (`
+SDRN_No`,
+`First_Name
+`, `Middle_Name`, `Last_Name`, `Email_ID`, `Contact_No`) VALUES
 ('101', 'Apurva', 'Swapnil', 'Shinde', 'apurva.karkhanis@rait.ac.in', 9821598302),
 ('102', 'Arjun', 'Khandu', 'Kale', 'kalearjun@gmail.com', 8989771132);
 
@@ -72,10 +95,14 @@ INSERT INTO `faculty` (`SDRN_No`, `First_Name`, `Middle_Name`, `Last_Name`, `Ema
 -- Table structure for table `fac_book`
 --
 
-CREATE TABLE `fac_book` (
-  `SDRN_No` varchar(30) NOT NULL,
-  `Book_ID` varchar(30) NOT NULL,
-  `Book_Name` varchar(80) NOT NULL,
+CREATE TABLE `fac_book`
+(
+  `SDRN_No` varchar
+(30) NOT NULL,
+  `Book_ID` varchar
+(30) NOT NULL,
+  `Book_Name` varchar
+(80) NOT NULL,
   `Issue_Date` date NOT NULL,
   `Return_Date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -84,9 +111,49 @@ CREATE TABLE `fac_book` (
 -- Dumping data for table `fac_book`
 --
 
-INSERT INTO `fac_book` (`SDRN_No`, `Book_ID`, `Book_Name`, `Issue_Date`, `Return_Date`) VALUES
-('101', '2', 'Data Warehousing ', '2020-07-01', '2020-07-08'),
-('102', '1', 'Computer Network ', '2020-07-01', '2020-07-08');
+INSERT INTO `fac_book` (`
+SDRN_No`,
+`Book_ID
+`, `Book_Name`, `Issue_Date`, `Return_Date`) VALUES
+('101', '2', 'Data Warehousing ', '2020-07-01', '2020-07-08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issue_request_stud`
+--
+
+CREATE TABLE `issue_request_stud`
+(
+  `Roll_No` varchar
+(30) NOT NULL,
+  `Book_ID` varchar
+(30) NOT NULL,
+  `Book_Name` varchar
+(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `issue_request_stud`
+--
+
+INSERT INTO `issue_request_stud` (`
+Roll_No`,
+`Book_ID
+`, `Book_Name`) VALUES
+('18CE8017', '1', 'Computer Network'),
+('18CE8017', '2', 'Data Warehousing '),
+('18CE8017', '2', 'Data Warehousing '),
+('18CE8017', '1', 'Computer Network'),
+('18CE8017', '1', 'Computer Network'),
+('18CE8017', '2', 'Data Warehousing '),
+('18CE8017', '1', 'Computer Network'),
+('18CE8017', '1', 'Computer Network'),
+('18CE8017', '1', 'Computer Network'),
+('18CE8017', '2', 'Data Warehousing '),
+('18CE8017', '1', 'Computer Network'),
+('18CE8017', '2', 'Data Warehousing '),
+('18CE8017', '2', 'Data Warehousing ');
 
 -- --------------------------------------------------------
 
@@ -94,22 +161,33 @@ INSERT INTO `fac_book` (`SDRN_No`, `Book_ID`, `Book_Name`, `Issue_Date`, `Return
 -- Table structure for table `requested_books`
 --
 
-CREATE TABLE `requested_books` (
-  `SDRN_No` varchar(30) NOT NULL,
-  `Faculty_Name` varchar(80) NOT NULL,
-  `Book_Name` varchar(80) NOT NULL,
-  `Author_Name` varchar(80) NOT NULL,
-  `Publication` varchar(80) NOT NULL,
-  `Date_Time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+CREATE TABLE `requested_books`
+(
+  `SDRN_No` varchar
+(30) NOT NULL,
+  `Faculty_Name` varchar
+(80) NOT NULL,
+  `Book_Name` varchar
+(80) NOT NULL,
+  `Author_Name` varchar
+(80) NOT NULL,
+  `Publication` varchar
+(80) NOT NULL,
+  `Date_Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON
+UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `requested_books`
 --
 
-INSERT INTO `requested_books` (`SDRN_No`, `Faculty_Name`, `Book_Name`, `Author_Name`, `Publication`, `Date_Time`) VALUES
-('101', 'Apurva Swapnil Shinde', 'Digital Signal Processing', 'A.Aanad Kumar', 'PHI Learning Pvt.Ltd', '2020-07-01 07:51:23'),
-('102', 'Arjun Khandu Kale', 'Real Time Operating System ', 'Jane W.S Liu', 'Pearson ', '2020-07-01 07:54:22');
+INSERT INTO `requested_books` (`
+SDRN_No`,
+`Faculty_Name
+`, `Book_Name`, `Author_Name`, `Publication`, `Date_Time`) VALUES
+('101', 'Apurva Swapnil Shinde', 'Digital Signal Processing', 'A.Aanad Kumar', 'PHI Learning Pvt.Ltd', '2020-07-01 02:21:23'),
+('102', 'Arjun Khandu Kale', 'Real Time Operating System ', 'Jane W.S Liu', 'Pearson ', '2020-07-01 02:24:22'),
+('101', 'Apurva Swapnil Shinde', 'Computer g', 'reema thareja', 'xyz', '2020-07-06 03:01:07');
 
 -- --------------------------------------------------------
 
@@ -117,25 +195,39 @@ INSERT INTO `requested_books` (`SDRN_No`, `Faculty_Name`, `Book_Name`, `Author_N
 -- Table structure for table `students`
 --
 
-CREATE TABLE `students` (
-  `Roll_No` varchar(30) NOT NULL,
-  `First_Name` varchar(30) NOT NULL,
-  `Middle_Name` varchar(30) NOT NULL,
-  `Last_Name` varchar(30) NOT NULL,
-  `Email_ID` varchar(30) NOT NULL,
-  `Contact_No` bigint(10) NOT NULL,
-  `Year` varchar(5) NOT NULL,
-  `Division` varchar(5) NOT NULL,
-  `Batch` varchar(5) NOT NULL
+CREATE TABLE `students`
+(
+  `Roll_No` varchar
+(30) NOT NULL,
+  `First_Name` varchar
+(30) NOT NULL,
+  `Middle_Name` varchar
+(30) NOT NULL,
+  `Last_Name` varchar
+(30) NOT NULL,
+  `Email_ID` varchar
+(30) NOT NULL,
+  `Contact_No` bigint
+(10) NOT NULL,
+  `Year` varchar
+(5) NOT NULL,
+  `Division` varchar
+(5) NOT NULL,
+  `Batch` varchar
+(5) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`Roll_No`, `First_Name`, `Middle_Name`, `Last_Name`, `Email_ID`, `Contact_No`, `Year`, `Division`, `Batch`) VALUES
+INSERT INTO `students` (`
+Roll_No`,
+`First_Name
+`, `Middle_Name`, `Last_Name`, `Email_ID`, `Contact_No`, `Year`, `Division`, `Batch`) VALUES
 ('18CE5006', 'Varsha', 'Sushil', 'Kedare', 'varsha.kedare2411@gmail.com', 8108307911, 'BE', 'B', 'B4'),
-('18CE5009', 'Reema', 'Anant', 'Kharat', 'reema.ak@gmail.com', 6677885131, 'BE', 'B', 'B4');
+('18CE5009', 'Reema', 'Anant', 'Kharat', 'reema.ak@gmail.com', 6677885131, 'BE', 'B', 'B4'),
+('18CE8017', 'Diksha', 'Ajaykumar', 'Nigam', 'nigamdiksha02@gmail.com', 8104148955, 'SE', 'C', 'C3');
 
 -- --------------------------------------------------------
 
@@ -143,10 +235,14 @@ INSERT INTO `students` (`Roll_No`, `First_Name`, `Middle_Name`, `Last_Name`, `Em
 -- Table structure for table `stud_book`
 --
 
-CREATE TABLE `stud_book` (
-  `Roll_No` varchar(30) NOT NULL,
-  `Book_ID` varchar(30) NOT NULL,
-  `Book_Name` varchar(80) NOT NULL,
+CREATE TABLE `stud_book`
+(
+  `Roll_No` varchar
+(30) NOT NULL,
+  `Book_ID` varchar
+(30) NOT NULL,
+  `Book_Name` varchar
+(80) NOT NULL,
   `Issue_Date` date NOT NULL,
   `Return_Date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -155,9 +251,14 @@ CREATE TABLE `stud_book` (
 -- Dumping data for table `stud_book`
 --
 
-INSERT INTO `stud_book` (`Roll_No`, `Book_ID`, `Book_Name`, `Issue_Date`, `Return_Date`) VALUES
+INSERT INTO `stud_book` (`
+Roll_No`,
+`Book_ID
+`, `Book_Name`, `Issue_Date`, `Return_Date`) VALUES
 ('18CE5009', '1', 'Computer Network ', '2020-07-01', '2020-07-08'),
-('18CE5006', '2', 'Data Warehousing ', '2020-07-01', '2020-07-08');
+('18CE5006', '2', 'Data Warehousing ', '2020-07-01', '2020-07-08'),
+('18CE8017', '1', 'Computer Network', '2020-06-27', '2020-07-04'),
+('18CE8017', '1', 'Computer Network', '2020-07-06', '2020-07-13');
 
 --
 -- Indexes for dumped tables
@@ -167,39 +268,56 @@ INSERT INTO `stud_book` (`Roll_No`, `Book_ID`, `Book_Name`, `Issue_Date`, `Retur
 -- Indexes for table `books`
 --
 ALTER TABLE `books`
-  ADD PRIMARY KEY (`Book_ID`);
+ADD PRIMARY KEY
+(`Book_ID`);
 
 --
 -- Indexes for table `faculty`
 --
 ALTER TABLE `faculty`
-  ADD PRIMARY KEY (`SDRN_No`);
+ADD PRIMARY KEY
+(`SDRN_No`);
 
 --
 -- Indexes for table `fac_book`
 --
 ALTER TABLE `fac_book`
-  ADD KEY `Book_ID` (`Book_ID`),
-  ADD KEY `SDRN_No` (`SDRN_No`);
+ADD KEY `Book_ID`
+(`Book_ID`),
+ADD KEY `SDRN_No`
+(`SDRN_No`);
+
+--
+-- Indexes for table `issue_request_stud`
+--
+ALTER TABLE `issue_request_stud`
+ADD KEY `Roll_No`
+(`Roll_No`),
+ADD KEY `Book_ID`
+(`Book_ID`);
 
 --
 -- Indexes for table `requested_books`
 --
 ALTER TABLE `requested_books`
-  ADD KEY `SDRN_No` (`SDRN_No`);
+ADD KEY `SDRN_No`
+(`SDRN_No`);
 
 --
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`Roll_No`);
+ADD PRIMARY KEY
+(`Roll_No`);
 
 --
 -- Indexes for table `stud_book`
 --
 ALTER TABLE `stud_book`
-  ADD KEY `Roll_No` (`Roll_No`),
-  ADD KEY `Book_ID` (`Book_ID`);
+ADD KEY `Roll_No`
+(`Roll_No`),
+ADD KEY `Book_ID`
+(`Book_ID`);
 
 --
 -- Constraints for dumped tables
@@ -209,21 +327,42 @@ ALTER TABLE `stud_book`
 -- Constraints for table `fac_book`
 --
 ALTER TABLE `fac_book`
-  ADD CONSTRAINT `fac_book_ibfk_1` FOREIGN KEY (`Book_ID`) REFERENCES `books` (`Book_ID`),
-  ADD CONSTRAINT `fac_book_ibfk_2` FOREIGN KEY (`SDRN_No`) REFERENCES `faculty` (`SDRN_No`);
+ADD CONSTRAINT `fac_book_ibfk_1` FOREIGN KEY
+(`Book_ID`) REFERENCES `books`
+(`Book_ID`),
+ADD CONSTRAINT `fac_book_ibfk_2` FOREIGN KEY
+(`SDRN_No`) REFERENCES `faculty`
+(`SDRN_No`);
+
+--
+-- Constraints for table `issue_request_stud`
+--
+ALTER TABLE `issue_request_stud`
+ADD CONSTRAINT `issue_request_stud_ibfk_1` FOREIGN KEY
+(`Roll_No`) REFERENCES `students`
+(`Roll_No`),
+ADD CONSTRAINT `issue_request_stud_ibfk_2` FOREIGN KEY
+(`Book_ID`) REFERENCES `books`
+(`Book_ID`);
 
 --
 -- Constraints for table `requested_books`
 --
 ALTER TABLE `requested_books`
-  ADD CONSTRAINT `requested_books_ibfk_1` FOREIGN KEY (`SDRN_No`) REFERENCES `faculty` (`SDRN_No`);
+ADD CONSTRAINT `requested_books_ibfk_1` FOREIGN KEY
+(`SDRN_No`) REFERENCES `faculty`
+(`SDRN_No`);
 
 --
 -- Constraints for table `stud_book`
 --
 ALTER TABLE `stud_book`
-  ADD CONSTRAINT `stud_book_ibfk_1` FOREIGN KEY (`Roll_No`) REFERENCES `students` (`Roll_No`),
-  ADD CONSTRAINT `stud_book_ibfk_2` FOREIGN KEY (`Book_ID`) REFERENCES `books` (`Book_ID`);
+ADD CONSTRAINT `stud_book_ibfk_1` FOREIGN KEY
+(`Roll_No`) REFERENCES `students`
+(`Roll_No`),
+ADD CONSTRAINT `stud_book_ibfk_2` FOREIGN KEY
+(`Book_ID`) REFERENCES `books`
+(`Book_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
